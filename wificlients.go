12677a9d21bd4645f179edac7517271d86f94589
Uwe Girlich <Uwe.Girlich@gmail.com>
2021-06-14 20:56:05 +0200

    print format generalized

--- wificlients.go
+++ wificlients.go
@@ -66,6 +66,24 @@ type Eap225StatusClientUser struct {
     LimitDownloadUnit int    `yaml:"limit_download_unit"`
 }
 
+type WiFiParam struct {
+    Radio      int    `yaml:"Radio"`
+    RSSI       int    `yaml:"RSSI"`
+    Rate       string `yaml:"Rate"`
+}
+
+type NetworkClient struct {
+    Hostname   string `yaml:"hostname"`
+    MAC        string `yaml:"MAC"`
+    IP         string `yaml:"IP"`
+    Down       int    `yaml:"Down"`
+    Up         int    `yaml:"Up"`
+    ActiveTime string `yaml:"ActiveTime"`
+    LinkType   string `yaml:"linktype"`
+    Upstream   string `yaml:"Upstream"`
+    WiFi       WiFiParam `yaml:"WiFi,omitempty"`
+}
+
 func eap225_get(credentials *Eap225Credential, clients *Eap225StatusClientUsers) {
   // Parse URL
   u, err := url.Parse("http://" + credentials.Host + "/")
@@ -117,11 +135,41 @@ func eap225_get(credentials *Eap225Credential, clients *Eap225StatusClientUsers)
 
   // Decode the JSON document
   json.NewDecoder(resp.Body).Decode(clients)
+}
 
-  // Normalize MAC
+func eap225_to_network(credentials *Eap225Credential, clients *Eap225StatusClientUsers, networkClients *[]NetworkClient) {
   for i, _ := range clients.Data {
-    clients.Data[i].MAC = strings.ToLower(clients.Data[i].MAC)
-    clients.Data[i].MAC = strings.Replace(clients.Data[i].MAC, "-", ":", 5)
+    var nc NetworkClient
+
+    // Copy Hostname verbatim
+    nc.Hostname = clients.Data[i].Hostname
+
+    // Normalize MAC
+    nc.MAC = strings.Replace(strings.ToLower(clients.Data[i].MAC), "-", ":", 5)
+
+    // Copy IP verbatim
+    nc.IP = clients.Data[i].IP
+
+    // Linktype is WiFi
+    nc.LinkType = "IEEE802_11"
+
+    nc.WiFi.Radio = clients.Data[i].Radio
+    nc.WiFi.RSSI = clients.Data[i].RSSI
+    nc.WiFi.Rate = clients.Data[i].Rate
+
+    // Download bytes
+    nc.Down = clients.Data[i].Down
+
+    // Upload bytes
+    nc.Up = clients.Data[i].Up
+
+    // Upstream is the Access Point
+    nc.Upstream = credentials.Host
+
+    // TODO: ActiveTime must be converted to seconds
+    nc.ActiveTime = clients.Data[i].ActiveTime
+
+    *networkClients = append(*networkClients, nc)
   }
 }
 
@@ -136,12 +184,15 @@ func main() {
 
   var multiEap225StatusClientUsers MultiEap225StatusClientUsers
   multiEap225StatusClientUsers.AccessPoints = make([]AccessPoint, len(credentials.Eap225s))
+  var NetworkClients []NetworkClient
   for i := 0 ; i<len(credentials.Eap225s) ; i++ {
     multiEap225StatusClientUsers.AccessPoints[i].Name=credentials.Eap225s[i].Host
     eap225_get(&credentials.Eap225s[i],&(multiEap225StatusClientUsers.AccessPoints[i].Data))
+    eap225_to_network(&credentials.Eap225s[i],
+      &(multiEap225StatusClientUsers.AccessPoints[i].Data),
+      &NetworkClients)
   }
-
-  multiEap225StatusClientUsersB, _ := yaml.Marshal(&multiEap225StatusClientUsers)
-    fmt.Println(string(multiEap225StatusClientUsersB))
+  networkClientsB, _ := yaml.Marshal(&NetworkClients)
+  fmt.Println(string(networkClientsB))
 }
 
